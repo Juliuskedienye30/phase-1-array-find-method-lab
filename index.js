9bc005e2bfc0c92b6693f52ded90cc5fa8fccf7c
Julius Kedienye <juliuskedienye61@gmail.com>
2025-06-16 22:04:24 +0300

    my first commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+  const winningYear = record.find(game => game.result === "W");
+  return winningYear ? winningYear.year : undefined;
+}
+
+// Export the function so it can be used in the test
+module.exports = superbowlWin;
